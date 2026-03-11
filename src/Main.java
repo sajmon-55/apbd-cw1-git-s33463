82697109027fa1509cd4782287fd23d540d9df1e
sajmon-55 <szgodlewski.pl@gmail.com>
2026-03-11 19:09:40 +0100

    Dodanie prostych metod

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+    public static int calc(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static int diff(int a, int b, int c) {
+        return a - b - c;
+    }
 }
